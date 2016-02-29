55091f452cacb3267241f536d988c1383f7096f9
sybil <sybil.deboin@gmail.com>
2016-02-29 00:35:52 -0500

    Clean the code

--- spec/lita/handlers/slack_standup_spec.rb
+++ spec/lita/handlers/slack_standup_spec.rb
@@ -40,12 +40,11 @@ describe Lita::Handlers::SlackStandup, lita_handler: true do
   end
 
   describe "when standup starts" do
-
     subject do 
-      send_message("!start standup", as: sybil, from: channel)
+      send_message("!standup start", as: sybil, from: channel)
     end
 
-    it "starts standup", vcr: {cassette_name: 'start_standup'} do
+    it "starts standup", vcr: {cassette_name: 'standup_start'} do
       [ "Hello <!channel> ! Le standup va commencer : )",
         "Bonjour <@zaratan> ! C'est à ton tour de parler."
       ].each do |text|
@@ -55,28 +54,28 @@ describe Lita::Handlers::SlackStandup, lita_handler: true do
       subject 
     end
 
-    it "annoys people only on working days" do
-      #standup test that it's called on weekday
-    end
+    context "with pre-filled report" do
+      before do 
+        send_message("!standup report My standup report for testing purposes", as: sybil, from: channel)
+      end
 
-    context "with pre-filled reports" do
-      it "displays known reports", vcr: {cassette_name: 'standup_prefilled'} do
-        send_message("!standup My standup report for testing purposes", as: sybil, from: channel)
-
+      it "displays known reports", vcr: {cassette_name: 'standup_one_report_prefilled'} do
 	[ "Hello <!channel> ! Le standup va commencer : )",
 	  "sybil a déjà renseigné son standup : \n My standup report for testing purposes",
 	  "Bonjour <@zaratan> ! C'est à ton tour de parler."
 	].each do |text|
 	  expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with({:channel=>channel, :text=>text, :as_user=>true})
 	end
-
         subject
       end
-      context "with all reports pre-filled", vcr: {cassette_name: 'standups_all_prefilled'} do
-        it "displays the standups and ends the meeting" do
-          send_message("!standup My standup report for testing purposes", as: sybil, from: channel)   
-          send_message("!standup My standup report for testing purposes", as: zaratan, from: channel)   
+    
+      context "with all reports pre-filled", vcr: {cassette_name: 'standup_all_reports_prefilled'} do
+        before do
+          send_message("!standup report My standup report for testing purposes", as: sybil, from: channel)   
+          send_message("!standup report My standup report for testing purposes", as: zaratan, from: channel)   
+        end
 
+        it "displays the standups and ends the meeting" do
  	  [ "Hello <!channel> ! Le standup va commencer : )",
 	    "zaratan a déjà renseigné son standup : \n My standup report for testing purposes",
 	    "sybil a déjà renseigné son standup : \n My standup report for testing purposes",
@@ -84,85 +83,81 @@ describe Lita::Handlers::SlackStandup, lita_handler: true do
 	  ].each do |text|
 	    expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with({:channel=>channel, :text=>text, :as_user=>true})       
           end
-
           subject
         end
       end
     end
   end
 
-  describe "!standup command", vcr: {cassette_name: 'standup'} do
+  describe "!standup report command", vcr: {cassette_name: 'standup_report'} do
     subject do 
-      send_message("!standup My standup report for testing purposes", as: sybil, from: channel)
+      send_message("!standup report My standup report for testing purposes", as: sybil, from: channel)
     end
 
-    context "when the user reports" do
-      it "saves the report" do
-        subject
-        expect(replies.last).to eq("Ton standup est enregistré. Merci :)")
-      end
+    it "saves the report" do
+      subject
+      expect(replies.last).to eq("Ton standup est enregistré. Merci :)")
     end
   end
 
-  describe "!next standup command" do
+  describe "!standup mext command" do
     subject do
-      send_message("!next standup", as: sybil, from: channel)
+      send_message("!standup next", as: sybil, from: channel)
     end
 
-    context "when its called oustide of standups", vcr: {cassette_name: 'next_standup_refused'} do
+    context "when its called oustide of standups", vcr: {cassette_name: 'standup_next_refused'} do
       it "is not available" do
         expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with({:channel=>channel, :text=>"La commande n'est pas disponible en dehors d'un standup.", :as_user=>true})
-       
         subject  
       end
     end
 
-    context "when it's called during standup", vcr: {cassette_name: 'next_standup'} do
+    context "when it's called during standup", vcr: {cassette_name: 'standup_next'} do
+      before do 
+        send_message("!standup start", as: sybil, from: channel)
+      end
+        
       it "asks the next user to report" do
-        send_message("!start standup", as: sybil, from: channel)
- 
         expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with({:channel=>channel, :text=>"Bonjour <@sybil> ! C'est à ton tour de parler.", :as_user=>true})
-       
         subject  
       end
     end
   end
 
-  describe "!ignore user command", vcr: {cassette_name: 'ignore_user'} do
+  describe "!standup ignore user command", vcr: {cassette_name: 'standup_ignore'} do
+
+    before do
+      send_message("!standup start", as: sybil, from: channel)
+    end
 
     subject do 
-      send_message("!ignore sybil", as: sybil, from: channel)
-      send_message("!next standup", as: sybil, from: channel)
+      send_message("!standup ignore sybil", as: sybil, from: channel)
+      send_message("!standup next standup", as: sybil, from: channel)
     end
 
     it "ignores an user" do
-      send_message("!start standup", as: sybil, from: channel)
-
       expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with({:channel=>channel, :text=>"Et voilà ! C'est bon pour aujourd'hui. Merci tout le monde :parrot:", :as_user=>true})
-
       subject 
       expect(replies.last).to eq("<@sybil> est désormais ignoré jusqu'à nouvel ordre.")
-    
     end
   end
 
-  describe "!unignore user command", vcr: {cassette_name: 'unignore_user'} do
+  describe "!standup unignore user command", vcr: {cassette_name: 'standup_unignore'} do
+
+    before do
+      send_message("!standup start", as: sybil, from: channel)
+      send_message("!standup ignore sybil", as: sybil, from: channel)
+    end
 
     subject do 
-      send_message("!unignore sybil", as: sybil, from: channel)
-      send_message("!next standup", as: sybil, from: channel)
+      send_message("!standup unignore sybil", as: sybil, from: channel)
+      send_message("!standup next", as: sybil, from: channel)
     end
 
     it "unignores an user" do
-      send_message("!start standup", as: sybil, from: channel)
-      send_message("!ignore sybil", as: sybil, from: channel)
-
       expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with({:channel=>channel, :text=>"Bonjour <@sybil> ! C'est à ton tour de parler.", :as_user=>true})
-
       subject 
       expect(replies.last).to eq("<@sybil> est à nouveau inclus dans les standups.")
-    
     end
   end
-
 end
